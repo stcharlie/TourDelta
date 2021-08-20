0d6ba6f48eda3da1075e52fe0f779fbc382f3398
stcharlie <charlieakarekor@gmail.com>
2021-08-20 12:39:05 +0100

    Third commit

--- app/src/main/java/com/benjamin/tourdelta/MainActivity.kt
+++ app/src/main/java/com/benjamin/tourdelta/MainActivity.kt
@@ -2,10 +2,41 @@ package com.benjamin.tourdelta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.benjamin.tourdelta.databinding.ActivityMainBinding
+import fragments.BookFragment
+import fragments.HomeFragment
+import fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val bookFragment = BookFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(homeFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.search -> replaceFragment(searchFragment)
+                R.id.book -> replaceFragment(bookFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.bottom_nav_frame, fragment)
+
+            transaction.commit()
+        }
+
     }
 }
